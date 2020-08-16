64cab5a9d49446df7685b32d0c772bfd2af8f681
muhammadzhuhry <muhammadzhuhry@gmail.com>
2020-08-16 21:43:33 +0700

    create function submitForm

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
-alert('document ready')
+const form = document.querySelector('form');
+const input = document.querySelector('#searchBox')
+
+form.addEventListener('submit', submitForm);
+
+function submitForm(event) {
+  event.preventDefault();
+  const searchBox = input.value;
+  console.log(searchBox)
+}
