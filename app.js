81b33dbaa64e84851b83c899cd8f5f6ee5cb307f
muhammadzhuhry <muhammadzhuhry@gmail.com>
2020-08-16 22:15:48 +0700

    add env and create function getResult

--- app.js
+++ app.js
@@ -1,10 +1,33 @@
 const form = document.querySelector('form');
 const input = document.querySelector('#searchBox')
 
+const env = {
+  API_URL: 'http://www.omdbapi.com/',
+  API_KEY: 'f13d554d'
+};
+
 form.addEventListener('submit', submitForm);
 
-function submitForm(event) {
+async function submitForm(event) {
   event.preventDefault();
   const searchBox = input.value;
-  console.log(searchBox)
+  
+  try {
+    const results = await getResult(searchBox);
+    console.log(results);
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+async function getResult(searchBox) {
+  const uri = `${env.API_URL}?apikey=${env.API_KEY}&s=${searchBox}`;
+  
+   const response = await fetch(uri);
+   const data = await response.json();
+   if (data.Error) {
+     throw new Error(data.Error);
+   }
+
+   return data.Search;
 }
