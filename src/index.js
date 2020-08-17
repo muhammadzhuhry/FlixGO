4c6e12f0a571410bb69532883e0461f0d0b35b0a
muhammadzhuhry <muhammadzhuhry@gmail.com>
2020-08-17 20:04:13 +0700

    create index js

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import "regenerator-runtime";
+import "bootswatch/dist/solar/bootstrap.min.css";
+import "./styles/style.css";
+import "./script/components/nav-bar.js";
+import "./script/components/hero-text.js";
+import main from "./script/main.js";
+
+main();
